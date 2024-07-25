4165bb0f3440dd2c47a0261c57ae55d7bcad6461
Eric <eroija@gmail.com>
2024-07-25 10:28:43 +0300

    A Building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,24 @@
+export default class Building {
+  /**
+   * @param {number} sqft - size of building
+   * Implement a getter and setter
+   */
+  constructor(sqft) {
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+}

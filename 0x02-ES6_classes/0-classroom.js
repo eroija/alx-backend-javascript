1c8888efe88807e51bce9265c8d7741a95851322
Eric <eroija@gmail.com>
2024-07-25 09:54:01 +0300

    You used to attend a place like this at some point

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,13 @@
+/**
+ * Represents class room.
+ */
+export default class ClassRoom {
+  /**
+   * Creates a new {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - The maximum number of
+   * students in the class.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}

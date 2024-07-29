709cb23917beb4107d4f08443ae8240301be1b66
Eric <eroija@gmail.com>
2024-07-29 17:28:10 +0300

    Set data structure

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,8 @@
+/**
+ * Creates new Set from the elements in given array.
+ * @param {Array} array - array to create the Set from
+ * @return {Set} new Set containing elements from the array
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
